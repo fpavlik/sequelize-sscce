1d185887b19b62852b2b2230041d0d06d700218e
Filipp Pavlik <filipp@videoly.co>
2022-11-01 15:23:26 +0300

    feat: added test to issue

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,10 +1,10 @@
 import { DataTypes, Model } from 'sequelize';
 import { createSequelize6Instance } from '../setup/create-sequelize-instance';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import sinon from 'sinon';
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
-export const testingOnDialects = new Set(['mssql', 'sqlite', 'mysql', 'mariadb', 'postgres', 'postgres-native']);
+export const testingOnDialects = new Set(['postgres']);
 
 // You can delete this file if you don't want your SSCCE to be tested against Sequelize 6
 
@@ -28,6 +28,7 @@ export async function run() {
   }, {
     sequelize,
     modelName: 'Foo',
+    tableName: 'foo'
   });
 
   // You can use sinon and chai assertions directly in your SSCCE.
@@ -36,6 +37,36 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  const result1 = await Foo.create({ name: 'TS foo' });
+  const firstId = result1.getDataValue('id');
+  
+  const result2 = await Foo.create({ name: 'TS foo2' });
+  const secondId = result2.getDataValue('id');
+
+  let isFirstQueryError = null;
+  let isSecondQueryError = null;
+
+  try {
+    await sequelize.query('SELECT * FROM foo WHERE id = ANY(ARRAY[:ids])', {
+      replacements: {
+        ids: [firstId, secondId, 3 ]
+      }
+    })
+  } catch (error) {
+    isFirstQueryError = error;
+  }
+  
+  try {
+    await sequelize.query("SELECT * FROM foo WHERE id = ANY(\'{:ids}\')", {
+      replacements: {
+        ids: [firstId, secondId, 3 ]
+      }
+    })
+  } catch (error) {
+    isSecondQueryError = error;
+  }
+
+  
+  assert.isNull(isFirstQueryError, 'First query with ARRAY[:ids]');
+  assert.isNull(isSecondQueryError, "Second query with \'{:ids}\'");
 }
